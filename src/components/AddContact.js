77c6d81be20c2720fa4b5e0b83bf9b83f25488fd
fasbrothers <nigmatovsanjar00@gmail.com>
2021-10-05 14:04:54 +0500

    add a function that creates a new student contact information

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import nextId from "react-id-generator";
+import { useHistory } from "react-router";
 
 function AddContact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const htmlId = nextId();
+  const contacts = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmail = contacts.find(
+      (contact) => contact.email === email && contact
+    );
+    const checkNumber = contacts.find(
+      (contact) => contact.number === parseInt(number)
+    );
+    if (!name || !email || !number) {
+      return toast.warning("Please, fill all of required places!");
+    }
+    if (checkEmail) {
+      return toast.error("This email is already using!");
+    }
+    if (checkNumber) {
+      return toast.error("This number is already using!");
+    }
+
+    const data = {
+      id: htmlId,
+      name,
+      number,
+      email,
+    };
+    dispatch({ type: "ADD_CONTACT", payload: data });
+    toast.success("Student succesfully added.");
+    history.push("/");
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -8,15 +52,23 @@ function AddContact() {
           <h1 className="display-3 text-center my-5 ">Add Contact</h1>
         </div>
         <div className="col-md-6 shadow mx-auto p-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input-group">
-              <input type="text" placeholder="Name" className="form-control" />
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="input-group">
               <input
                 type="email"
                 placeholder="Email"
                 className="form-control my-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="input-group">
@@ -24,6 +76,8 @@ function AddContact() {
                 type="number"
                 placeholder="Number"
                 className="form-control"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
               />
             </div>
             <div className="input-group d-grid ">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const contacts = useSelector((state) => state);
+
   return (
     <div className="container">
       <div className="row">
@@ -11,7 +14,38 @@ function Home() {
           </Link>
         </div>
         <div className="col-md-10 mx-auto">
-          <h1>Welcome to Contact App</h1>
+          <table className="table table-hover">
+            <thead className="bg-dark text-center text-white">
+              <tr>
+                <th scope="col">№</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Number</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody className="text-center">
+              {contacts.map((contact, id) => (
+                <tr key={id}>
+                  <td>{id + 1}</td>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.number}</td>
+                  <td>
+                    <Link
+                      to={`/edit/${contact.id}`}
+                      className="btn btn-sm btn-primary"
+                    >
+                      Edit
+                    </Link>
+                    <button className="btn btn-sm btn-danger mx-2">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
